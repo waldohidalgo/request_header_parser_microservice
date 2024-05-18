9211b78ee14260f85080a4880f6352fa225398ec
waldohidalgo <whidalgohp@gmail.com>
2024-05-18 16:25:50 -0400

    code ready

---
+++ api/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve("api", "views", "index.html"));
+});
+
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
